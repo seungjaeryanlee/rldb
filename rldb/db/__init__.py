13687702f93cb36607ea2f7e44aaa69989b28c6a
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-03-28 17:15:16 +0900

    Shorten module names (#19)

--- rldb/db/__init__.py
+++ rldb/db/__init__.py
@@ -1,11 +1,11 @@
-from .paper__deep_recurrent_q_learning_for_partially_observable_mdps import entries as drqn_entries
-from .paper__deep_reinforcement_learning_with_double_q_learning import entries as ddqn_entries
-from .paper__dueling_network_architectures_for_deep_reinforcement_learning import entries as dueling_entries
-from .paper__exploration_by_random_network_distillation import entries as rnd_entries
-from .paper__playing_atari_with_deep_reinforcement_learning import entries as dqn2013_entries
-from .paper__prioritized_experience_replay import entries as per_entries
-from .paper__proximal_policy_optimization_algorithms import entries as ppo_entries
-from .paper__trust_region_policy_optimization import entries as trpo_entries
+from .paper__drqn import entries as drqn_entries
+from .paper__ddqn import entries as ddqn_entries
+from .paper__dueling_dqn import entries as dueling_entries
+from .paper__rnd import entries as rnd_entries
+from .paper__dqn2013 import entries as dqn2013_entries
+from .paper__prioritized_dqn import entries as per_entries
+from .paper__ppo import entries as ppo_entries
+from .paper__trpo import entries as trpo_entries
 
 entries = (
     []
