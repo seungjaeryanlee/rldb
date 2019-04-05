b6cc04979e911aefa8d66b3f1f522cdfec92a604
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-06 03:03:39 +0900

    Remove score-source-title attribute (#43)
    * Remove score-source-title attributes
    
    * Abandon ALE paper
    
    * Remove ramining score-source-title attributes

--- rldb/db/paper__ddqn/algo__random/entries.py
+++ rldb/db/paper__ddqn/algo__random/entries.py
@@ -2,49 +2,41 @@ entries = [
     {
         'env-title': 'atari-berzerk',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 196.10,
     },
     {
         'env-title': 'atari-defender',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 1965.50,
     },
     {
         'env-title': 'atari-phoenix',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 1134.40,
     },
     {
         'env-title': 'atari-pitfall',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': -348.80,
     },
     {
         'env-title': 'atari-skiing',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': -15287.40,
     },
     {
         'env-title': 'atari-solaris',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 2047.20,
     },
     {
         'env-title': 'atari-surround',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': -9.70,
     },
     {
         'env-title': 'atari-yars-revenge',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 1476.90,
     },
 ]
