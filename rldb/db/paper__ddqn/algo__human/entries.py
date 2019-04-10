2c7a4b90053fe10547974418e69295d0ee0007a3
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-10 15:12:44 +0900

    Remove remaining score-source-title attributes (#46)

--- rldb/db/paper__ddqn/algo__human/entries.py
+++ rldb/db/paper__ddqn/algo__human/entries.py
@@ -2,49 +2,41 @@ entries = [
     {
         'env-title': 'atari-berzerk',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 2237.50,
     },
     {
         'env-title': 'atari-defender',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 14296.00,
     },
     {
         'env-title': 'atari-phoenix',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 6686.20,
     },
     {
         'env-title': 'atari-pitfall',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 5998.90,
     },
     {
         'env-title': 'atari-skiing',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': -3686.60,
     },
     {
         'env-title': 'atari-solaris',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 11032.60,
     },
     {
         'env-title': 'atari-surround',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 5.40,
     },
     {
         'env-title': 'atari-yars-revenge',
         'env-variant': 'Human start',
-        "score-source-title": "Massively Parallel Methods for Deep Reinforcement Learning",
         'score': 47135.20,
     },
 ]
