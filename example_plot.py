6a6989fe9261a36c885d3ea637c8a4f3e2490d54
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-11 14:53:25 +0900

    Use different colors in bar plot for human and random scores (#48)
    * Custom color for Human and Random
    
    * Update README plots

--- example_plot.py
+++ example_plot.py
@@ -12,11 +12,14 @@ def env_barplot(filter, plot_title, plot_name):
         key=lambda entry: entry['score'],
         reverse=True,
     )
-    labels, scores = entries_to_labels_scores(sorted_entries)
+    labels, scores, colors = entries_to_labels_scores(sorted_entries)
 
     # Draw bar plot
     plt.figure(figsize=(12, 8))
-    plt.barh(labels, scores)
+    bars = plt.barh(labels, scores, color=colors)
+    # Color 'Human' and 'Random' entries
+
+    # Add text
     plt.title(plot_title)
     plt.tight_layout()
     plt.savefig('docs/{}.png'.format(plot_name))
@@ -31,7 +34,8 @@ def entries_to_labels_scores(entries):
     nicknames = [entry['algo-nickname'] for entry in entries]
 
     labels = []
-    scores = []    
+    scores = []
+    colors = []
     for entry in entries:
         label = entry['algo-nickname']
 
@@ -39,10 +43,21 @@ def entries_to_labels_scores(entries):
         if nicknames.count(entry['algo-nickname']) > 1:
             label += ' (From {})'.format(entry['source-nickname'])
 
+        # Special color for 'Human' and 'Random'
+        
+        if entry['algo-title'] == 'Human':
+            color = 'red'
+        elif entry['algo-title'] == 'Random':
+            color = 'black'
+        else:
+            color = 'darkblue'
+
         labels.append(label)
         scores.append(entry['score'])
 
-    return labels, scores
+        colors.append(color)
+
+    return labels, scores, colors
 
 
 def main():
