71dc5982668000aaeda151dec780302c6f7ea5fa
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-04-12 15:27:57 +0900

    Add score labels to bar plots (#49)
    * Add smart labels for scores in barplot
    
    * Change smart label style
    
    * Update README plots

--- example_plot.py
+++ example_plot.py
@@ -15,12 +15,43 @@ def env_barplot(filter, plot_title, plot_name):
     labels, scores, colors = entries_to_labels_scores(sorted_entries)
 
     # Draw bar plot
-    plt.figure(figsize=(12, 8))
-    bars = plt.barh(labels, scores, color=colors)
-    # Color 'Human' and 'Random' entries
+    fig, ax = plt.subplots(figsize=(12, 8))
+    bars = ax.barh(labels, scores, color=colors)
 
-    # Add text
+    # Add title
     plt.title(plot_title)
+
+    # Add score labels
+    switch_label_align = False # If true, put score label outside of barplot
+    for i, (bar, entry, score) in enumerate(zip(bars, sorted_entries, scores)):
+
+        text = ax.text(
+            x=bar.get_x() + bar.get_width() - 50,
+            y=bar.get_y() + bar.get_height() / 2,
+            s=str(score),
+            color='white',
+            fontweight='bold',
+            ha='right',
+            va='center',
+        )
+
+        # Check if putting score label outside of barplot would make it too long
+        bar_x1 = bar.get_window_extent(renderer=fig.canvas.get_renderer()).x1
+        ax_x1 = ax.get_window_extent(renderer=fig.canvas.get_renderer()).x1
+        text_width = text.get_window_extent(renderer=fig.canvas.get_renderer()).width
+        if switch_label_align or bar_x1 + text_width < ax_x1:
+            switch_label_align = True
+            text.set_x(bar.get_x() + bar.get_width() + 50)
+            text.set_ha('left')
+
+            if entry['algo-title'] == 'Human':
+                color = 'red'
+            elif entry['algo-title'] == 'Random':
+                color = 'black'
+            else:
+                color = 'darkblue'
+            text.set_color(color)
+
     plt.tight_layout()
     plt.savefig('docs/{}.png'.format(plot_name))
     # plt.show()
@@ -44,7 +75,6 @@ def entries_to_labels_scores(entries):
             label += ' (From {})'.format(entry['source-nickname'])
 
         # Special color for 'Human' and 'Random'
-        
         if entry['algo-title'] == 'Human':
             color = 'red'
         elif entry['algo-title'] == 'Random':
