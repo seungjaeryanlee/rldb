8d094b061a71c32b356dd5a2c87d61adf6c10f35
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-05-14 18:13:10 +0900

    Implement median human-normalized score (#71)

--- rldb/normalization.py
+++ rldb/normalization.py
@@ -1,10 +1,12 @@
+from statistics import median
+
 from .find import find_all
 from .db import entries
 
 
 def get_human_normalized_score(entry):
     """
-    Return human-normalized score of the entry. 
+    Return human-normalized score of the entry.
 
     This normalization is well documented in the Rainbow paper.
     https://arxiv.org/abs/1710.02298
@@ -27,3 +29,20 @@ def get_human_normalized_score(entry):
         random_entries.sort(key=lambda entry: entry['score'], reverse=True)
 
     return (entry['score'] - random_entries[0]['score']) / (human_entries[0]['score'] - random_entries[0]['score'])
+
+
+def get_atari_median_human_normalized_score(algo_title):
+    """
+    Return median of human-normalized score of the algorithm for atari environments.
+
+    This normalization is well documented in the Rainbow paper.
+    https://arxiv.org/abs/1710.02298
+    """
+    normalized_scores = []
+    algo_entries = find_all({ 'algo-title': algo_title })
+    for algo_entry in algo_entries:
+        if algo_entry['env-title'][:5] == 'atari':
+            score = get_human_normalized_score(algo_entry)
+            normalized_scores.append(score)
+    
+    return median(normalized_scores)
