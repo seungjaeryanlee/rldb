87d1d39d6cadee54a395dbb0d65c0a7938e42391
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-12-18 06:02:15 +0900

    Add merging script for two extracted dataframes

---
+++ parsers/merger.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from pdf_parser_1312_5602 import PDFParser_1312_5602
+from pdf_parser_1911_08265 import PDFParser_1911_08265
+
+
+def merge(parsers):
+    dfs = [parser.df for parser in parsers]
+    merged_df = pd.concat(dfs, axis=1)
+
+    return merged_df
+
+
+if __name__ == "__main__":
+    parsers = [
+        PDFParser_1312_5602(),
+        PDFParser_1911_08265(),
+    ]
+
+    merged_df = merge(parsers)
+    print(merged_df)
