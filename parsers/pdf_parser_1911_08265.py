f80a4f5d04c90930e75fc75bb50030c084361810
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-12-17 00:50:47 -0500

    Add PDFParser for 1911.08265

---
+++ parsers/pdf_parser_1911_08265.py
@@ -0,0 +1,20 @@
+import camelot
+
+from pdf_parser import PDFParser
+
+
+class PDFParser_1911_08265(PDFParser):
+    def _preprocess(self):
+        tables = camelot.read_pdf("../pdfs/1911.08265.pdf", pages="17,18", flavor="stream")
+        df = tables[0].df
+        df = df.iloc[:-1].drop(columns=[7])
+
+        report = tables[0].parsing_report
+
+        self.df, self.report = df, report
+
+
+if __name__ == "__main__":
+    parser = PDFParser_1911_08265()
+    print(parser.df)
+    print(parser.report)
