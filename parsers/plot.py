fde5c11d49c1964e4078fbac5e26d22cc585af18
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-12-19 06:17:03 +0900

    Improve plot design and labels

--- parsers/plot.py
+++ parsers/plot.py
@@ -5,7 +5,21 @@ import plotly.graph_objects as go
 def plot_single_env(df, env_name):
     df = df.dropna(subset=[env_name])
     df = df.sort_values(by=[env_name], ascending=True)
-    fig = go.Figure([go.Bar(x=df["agent_name"]+": "+df["paper_title"], y=df[env_name])])
+    fig = go.Figure([go.Bar(
+        x=df["agent_nickname"] + df["paper_title"],
+        y=df[env_name],
+        text=df[env_name],
+        textposition='auto',
+        hovertext="<b>Paper: </b>" + df["paper_title"] + "<br><b>Agent: </b>" + df["agent_name"],
+    )])
+    fig.update_layout(
+        barmode='group',
+        xaxis=dict(
+            tickmode="array",
+            tickvals=df["agent_nickname"] + df["paper_title"],
+            ticktext=df["agent_nickname"],
+        ),
+    )
     fig.show()
 
 
